5d3b05ea377d949611fd761eab1527df9874fe43
Laura Sofia Heimann <hello@laura.media>
2023-01-31 20:02:46 +0100

    Removed unused events

--- scripts/util/event-definition.js
+++ scripts/util/event-definition.js
@@ -12,31 +12,3 @@
 //          https://developer.valvesoftware.com/wiki/Dota_2_Workshop_Tools/Panorama/Events
 //
 //--------------------------------------------------------------------------------------------------
-
-$.DefineEvent(
-    'HideContentPanel',
-    0,
-    'no args',
-    'Hide all the main menu content panels and show the default home dashboard',
-);
-$.DefineEvent('ShowContentPanel', 0, 'no args', 'Show a content panel');
-$.DefineEvent('ReloadBackground', 0, '', 'Reloads the main menu background');
-$.DefineEvent(
-    'MainMenuTabShown',
-    1,
-    'tabid',
-    'Alert main menu tabs when they are shown, in case there is a data update needed',
-);
-$.DefineEvent('MainMenuTabHidden', 1, 'tabid', 'Fired when a main menu tab is closed');
-
-$.DefineEvent('SettingsNavigateToPanel', 2, 'category, settingPanel', 'Navigates to a setting by panel handle');
-$.DefineEvent('SettingsSave', 0, 'Save the settings out to file (host_writeconfig)');
-
-$.DefineEvent('RefreshLobbyList', 0, '', 'Send a refresh request for the lobby list');
-$.DefineEvent('Lobby_SetMaxPlayers', 1, 'maxplayers', 'Sets the max players of your lobby');
-
-$.DefineEvent('MapSelector_OnLoaded', 0);
-$.DefineEvent('MapSelector_ShowConfirmCancelDownload', 1);
-$.DefineEvent('HudLeaderboards_ForceClose', 0, '', 'Force close the leaderboards');
-$.DefineEvent('ColorPickerSave', 1, 'color');
-$.DefineEvent('ColorPickerCancel', 0);
